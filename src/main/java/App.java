18d703dfdaacc47d28b11e4fddde0f0746cd4447
EvgVN <kolombo@bk.ru>
2023-10-25 00:37:06 +0400

    1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld analogBean = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(analogBean.getMessage());
+        System.out.println(bean == analogBean);
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getMessage());
+        Cat otherBeanCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(otherBeanCat.getMessage());
+        System.out.println(beanCat == otherBeanCat);
     }
 }
